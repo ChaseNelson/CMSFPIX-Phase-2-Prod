bda70e272086771a47c3f08e6a66ca6c57b7d273
Chase Nelson <chase.a.nelson@gmail.com>
2017-08-01 10:29:47 -0400

    gulpfile setup

--- gulpfile.js
+++ gulpfile.js
@@ -10,3 +10,75 @@ var gulp         = require('gulp'),
     minifyInline = require('gulp-minify-inline'),
     imagemin     = require('gulp-imagemin'),
     rename       = require('gulp-rename');
+
+//*************************************
+//     SCRIPT TASK
+//*************************************
+gulp.task('scripts', function(cb) {
+  pump([
+    gulp.src(['./app/js/**/*.js', '!./app/js/**/*.min.js']),
+    plumber(),
+    rename({suffix:'.min'}),
+    uglify(),
+    gulp.dest('./app/js/**/')
+  ], cb);
+});
+
+//*************************************
+//     HTML TASK
+//*************************************
+gulp.task('html', function(cb) {
+  pump([
+    gulp.src('./app/**/*.html'),
+    pipe(reload({stream:true}))
+  ], cb);
+});
+
+//*************************************
+//     CSS TASK
+//*************************************
+gulp.task('css', function(cb) {
+  pump([
+    gulp.src('./app/css/*.css', '!./app/css/*.min.css'),
+    rename({suffix:'.min'}),
+    uglifycss(),
+    gulp.dest('./css')
+  ], cb);
+})
+
+//*************************************
+//     IMAGE TASK
+//*************************************
+gulp.task('image', function(cb) {
+  pump([
+    gulp.src('./app/images/*'),
+    imagemin(),
+    gulp.dest('./app/images/*')
+  ], cb);
+});
+
+//*************************************
+//     BROWSER-SYNC TASK
+//*************************************
+gulp.task('browser-sync', function() {
+  browserSync.init({
+    server:{
+      baseDir: "./app"
+    }
+  });
+});
+
+//*************************************
+//     WATCH TASK
+//*************************************
+gulp.task('watch', function() {
+  gulp.watch('./app/js/**/*.js', ['scripts']);
+  gulp.watch('./app/**/*.html', ['html']);
+  gulp.watch('./app/css/*.css', ['css']);
+  gulp.watch('./app/images/*', ['image']);
+})
+
+//*************************************
+//     DEFAULT TASK
+//*************************************
+gulp.task('default', ['scripts', 'css', 'image', 'html', 'browser-sync', 'watch']);
