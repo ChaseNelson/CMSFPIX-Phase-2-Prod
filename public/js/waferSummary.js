991a27a78382b86f50f347730145db862423c93c
Chase Nelson <chase.a.nelson@gmail.com>
2017-08-09 10:58:22 -0400

    Works! makes arrays for React

--- public/js/waferSummary.js
+++ public/js/waferSummary.js
@@ -1,17 +1,38 @@
-var id;
+var id = parent.document.URL.substring(parent.document.URL.indexOf('id=') + 3, parent.document.URL.length);
+database = firebase.database();
+var ref = database.ref('Wafer/' + id);
+ref.on('value', gotData, errData);
 
-function getData(key) {
-  id = key;
-  database = firebase.database();
-  var ref = database.ref('Wafer');
-  ref.on('value', gotData, errData);
-}
+var lastEdit;
+var name;
+var keyArray   = [
+  "Status",
+  "Thickness",
+  "Vendor"
+];
+var valueArray = [];
+var fieldArray = [
+  "status",
+  "thickness",
+  "vendor"
+];
+
 
 function gotData(data) {
-  var value = data.val();
-  console.log(value[id]);
+  value = data.val();
+  name = value.name;
+  lastEdit = value.lastEdit;
+  for (var i = 0; i < fieldArray.length; i++) {
+    valueArray[i] = value[fieldArray[i]];
+  }
 }
 
 function errData(data) {
   console.log("ERROR :: " + data);
+}
+
+function updatePart(field, val) {
+  var obj = value;
+  obj[field] = val;
+  ref.set(obj);
 }
