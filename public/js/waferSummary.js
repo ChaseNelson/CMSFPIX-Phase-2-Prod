b94d5948177ac0cce8bb9ed2a60c2f9039056171
Chase Nelson <chase.a.nelson@gmail.com>
2017-08-10 11:48:08 -0400

    React Works!!

--- public/js/waferSummary.js
+++ public/js/waferSummary.js
@@ -25,6 +25,7 @@ function gotData(data) {
   for (var i = 0; i < fieldArray.length; i++) {
     valueArray[i] = value[fieldArray[i]];
   }
+  renderReact();
 }
 
 function errData(data) {
@@ -34,5 +35,15 @@ function errData(data) {
 function updatePart(field, val) {
   var obj = value;
   obj[field] = val;
+  var time = new Date();
+  var h = time.getHours();
+  var m = time.getMinutes();
+  var s = time.getSeconds();
+  var dd = time.getDate();
+  var mm = time.getMonth() + 1;
+  var yyyy = time.getFullYear();
+  if (dd < 10) dd = '0' + dd;
+  if (mm < 10) mm = '0' + mm;
+  obj['lastEdit'] = mm + '-' + dd + '-' + yyyy + ' ' + h + ':' + m + ':' + s;
   ref.set(obj);
 }
