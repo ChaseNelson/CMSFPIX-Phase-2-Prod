60dbc1b1dd17f670a07f3e4f9667dd4e9f3d8665
Chase Nelson <chase.a.nelson@gmail.com>
2017-08-17 14:11:21 -0400

    Working create sensor page

--- public/js/createPart.js
+++ public/js/createPart.js
@@ -2,6 +2,29 @@ function createHDI() {
   var name = document.getElementById('name').value;
   var location = document.getElementById('location').value;
   var status = document.getElementById('status').value;
+  var time = getTime();
+  data = {
+    lastEdit: time,
+    location: location,
+    name: name,
+    partType: 'HDI',
+    status: status,
+  };
+  firebase.database().ref('HDI').push(data);
+}
+
+function createSensor() {
+  var name = document.getElementById('name').value;
+  var time = getTime();
+  data = {
+    lastEdit: time,
+    name: name,
+    partType: 'Sensor'
+  };
+  firebase.database().ref('Sensor').push(data);
+}
+
+function getTime() {
   var time = new Date();
   var h = time.getHours();
   var m = time.getMinutes();
@@ -11,13 +34,6 @@ function createHDI() {
   var yyyy = time.getFullYear();
   if (dd < 10) dd = '0' + dd;
   if (mm < 10) mm = '0' + mm;
-  time = mm + '-' + dd + '-' + yyyy + ' ' + h + ':' + m + ':' + s;
-  data = {
-    lastEdit: time,
-    location: location,
-    name: name,
-    partType: 'HDI',
-    status: status,
-  }
-  firebase.database().ref('HDI').push(data);
+  var time = mm + '-' + dd + '-' + yyyy + ' ' + h + ':' + m + ':' + s;
+  return time;
 }
